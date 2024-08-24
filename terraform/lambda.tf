08eb1d658f41f1293f9081ad032b1db8dc9e7788
APtheepan <APtheepan@example.com>
2024-08-24 13:40:03 -0400

    troubleshoot GHA

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -82,11 +82,11 @@ resource "aws_lambda_function" "visitorcount" {
   # If the file is not in the current working directory you will need to include a
   # path.module in the filename.
   #filename         = "backend/visitorcount.zip"
-  filename         = "https://github.com/APtheepan/aws_cloud_resume_challenge/blob/1675ee8d5efec1e9663b05877718a75f24af5f1c/backend/visitorcount.zip"
+  filename         = "/home/theepan/aws_cloud_resume_challenge/backend/visitorcount.zip"
   function_name    = "visitorcount"
   role             = aws_iam_role.iam_for_lambda.arn
   handler          = "visitorcount.lambda_handler"
-  #source_code_hash = data.archive_file.lambda.output_base64sha256
+  source_code_hash = filebase64sha256("/home/theepan/aws_cloud_resume_challenge/backend/visitorcount.zip")
   runtime          = "python3.8"
 
 }
