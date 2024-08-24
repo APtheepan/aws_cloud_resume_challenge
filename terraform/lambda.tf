ad6fd0dd53de54b074b415392e07e87dc1d34b2b
APtheepan <APtheepan@example.com>
2024-08-24 17:41:03 -0400

    troubleshoot GHA

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -64,15 +64,18 @@ resource "aws_iam_role" "iam_for_lambda" {
 */
 
 
-/*
+
 data "archive_file" "lambda" {
   type = "zip"
-  source_file = "/home/theepan/aws_cloud_resume_challenge/backend/visitorcount.py"
-  output_path = "/home/theepan/aws_cloud_resume_challenge/backend/visitorcount.zip"
+  output_path = "${path.module}/visitorcount.zip"
+  source {
+    content = "hello"
+    filename = "visitorcount.py"
+  }
   #source_file = "${path.module}/backend/visitorcount.py"
   #output_path = "${path.module}/backend/visitorcount.zip"
 }
-*/
+
 resource "aws_lambda_function_url" "lambda_url" {
   function_name      = aws_lambda_function.visitorcount.function_name
   authorization_type = "NONE"
@@ -81,7 +84,7 @@ resource "aws_lambda_function_url" "lambda_url" {
 resource "aws_lambda_function" "visitorcount" {
   # If the file is not in the current working directory you will need to include a
   # path.module in the filename.
-  filename         = "backend/visitorcount.zip"
+  filename         = "${data.archive_file.lambda.output_path}"
   #filename         = "/home/theepan/aws_cloud_resume_challenge/backend/visitorcount.zip"
   function_name    = "visitorcount"
   role             = aws_iam_role.iam_for_lambda.arn
