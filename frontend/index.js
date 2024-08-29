84ebf90044e86a46acaacf3dfc48370262e968a0
APtheepan <APtheepan@example.com>
2024-08-29 14:26:11 -0400

    Update lambda url in frontend

--- frontend/index.js
+++ frontend/index.js
@@ -1,5 +1,5 @@
 async function trigger() {
-    const apiUrl = "https://hqmknvmgmeokefkf5wtyv22mzq0vbtgc.lambda-url.us-east-1.on.aws/"; // Replace with your API Gateway URL
+    const apiUrl = "https://mrx2jywncxzp2v5ziitxisbjzi0rwuva.lambda-url.us-east-1.on.aws/"; // Replace with your API Gateway URL
 
     let response =  fetch(apiUrl)
 
