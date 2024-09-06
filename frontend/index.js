c1dc641d0797481f836a2c3bf98e0eb685322955
APtheepan <APtheepan@example.com>
2024-09-06 14:41:24 -0400

    Edit infra

--- frontend/index.js
+++ frontend/index.js
@@ -1,19 +1,25 @@
+
+var apiUrl = "https://ezvm5id6yqptwzehm5gmghhnem0trjen.lambda-url.us-east-1.on.aws/"; // Replace with your API Gateway URL
+
 async function trigger() {
-    const apiUrl = "https://ezvm5id6yqptwzehm5gmghhnem0trjen.lambda-url.us-east-1.on.aws/"; // Replace with your API Gateway URL
-
-     fetch(apiUrl)
-        .then(response => response.json()) // Parse the JSON from the response
-        .then(data => {
-        // Assuming 'body' is a property in the JSON response
-            document.querySelector('.counter-number').innerHTML = data.body; // Display the 'body' in the HTML
-     })
-        .catch(error => {
-             console.error('Error fetching data:', error);
-            document.querySelector('.counter-number').innerHTML = 'Failed to load data'; // Show error message
-        });
-
+    
+    let response =  fetch(apiUrl)
 }
 trigger();
+
+function getApi() {
+    fetch(apiUrl)
+    .then(response => response.json()) // Parse the JSON from the response
+    .then(data => {
+        // Assuming 'body' is a property in the JSON response
+        document.querySelector('.counter-number').innerHTML = data.body; // Display the 'body' in the HTML
+    })
+    .catch(error => {
+        console.error('Error fetching data:', error);
+        document.querySelector('.counter-number').innerHTML = 'Failed to load data'; // Show error message
+    });
+}
+
 
 var typed = new Typed('#typed', {
     strings: [
