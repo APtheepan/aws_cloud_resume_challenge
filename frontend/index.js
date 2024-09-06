682a2e7c889e2b626a42132ba023e8b1e5872096
APtheepan <APtheepan@example.com>
2024-09-06 14:34:40 -0400

    Edit infra

--- frontend/index.js
+++ frontend/index.js
@@ -2,11 +2,14 @@ async function trigger() {
     const apiUrl = "https://ezvm5id6yqptwzehm5gmghhnem0trjen.lambda-url.us-east-1.on.aws/"; // Replace with your API Gateway URL
 
     let response =  fetch(apiUrl)
-
+    .then(response => response.json()) // Parse the JSON from the response
     .then(data => {
-        // Assuming the 'body' is a property in the JSON response
-        console.log(data.body); // Access and process the body
-        document.querySelector('.counter-number').innerHTML = data.body;
+        // Assuming 'body' is a property in the JSON response
+        document.querySelector('.counter-number').innerHTML = data.body; // Display the 'body' in the HTML
+    })
+    .catch(error => {
+        console.error('Error fetching data:', error);
+        document.querySelector('.counter-number').innerHTML = 'Failed to load data'; // Show error message
     });
 
 }
