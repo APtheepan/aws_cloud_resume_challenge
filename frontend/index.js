d908acb85eb2292233b0f17a4a1b4ce2458c0b2c
APtheepan <APtheepan@example.com>
2024-09-07 22:45:48 -0400

    Edit infra

--- frontend/index.js
+++ frontend/index.js
@@ -4,18 +4,7 @@ async function trigger() {
     const apiUrl  = "https://ppdre6h5gkzqqmonubgbzhi24i0qvfwt.lambda-url.us-east-1.on.aws/"
     let response =  fetch(apiUrl)
 }
-
-const counter = document.querySelector(".counter-number");
-async function getApi() {
-    const apiUrl  = "https://ppdre6h5gkzqqmonubgbzhi24i0qvfwt.lambda-url.us-east-1.on.aws/"
-    let response = await fetch(apiUrl);
-    let data = await response.json();
-    counter.innerHTML = `Views: ${data}`;
- 
-}
-
 trigger();
-getApi();
 
 var typed = new Typed('#typed', {
     strings: [
