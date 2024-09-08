ecf518f59ad7e0669e8ea28bfaf1e8a93a95555a
APtheepan <APtheepan@example.com>
2024-09-08 16:50:45 -0400

    Edit infra

--- frontend/index.js
+++ frontend/index.js
@@ -1,9 +1,9 @@
 const counter = document.querySelector(".counter-number");
 async function updateCounter() {
-    let response = await fetch(
+    const response = await fetch(
         "https://fd7txc5dxlvcuu2uxmm4u4jkbm0eehjd.lambda-url.us-east-1.on.aws/"
     );
-    let data = await response.json();
+    const data = await response.json();
     counter.innerHTML = `Views: ${data}`;
 }
 updateCounter();
